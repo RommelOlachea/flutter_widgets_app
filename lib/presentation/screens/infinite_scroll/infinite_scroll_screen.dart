59a00a66b47a5a61157722950d78b0555bd57e7b
Rommel Olachea <msc.rommel.olachea@gmail.com>
2023-08-07 14:27:19 -0700

    Indicador de carga - refresh indicator

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,6 +13,62 @@ class InfiniteScrollScreen extends StatefulWidget {
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
   List<int> imagesIds = [1, 2, 3, 4, 5];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true; //cuando creamos la pagina, esta montado el widget
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      if (scrollController.position.pixels + 500 >=
+          scrollController.position.maxScrollExtent) {
+        //load next page
+        loadNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false; //marcamos como desmontado el widget
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    addFiveImages();
+    isLoading = false;
+    //todo: revisar si esta montado el componente / widget
+
+    if (!isMounted)
+      return; //si no esta montado no lo refrescamos, lo hacemos asi, porque estamos en una funcion asincrona
+    //si nos salimos de la pagina y regresamos y esta la funcion asincrona ejecutandose no puede refrescar el control
+    //porque esta destruido
+    setState(() {});
+
+    //todo: mover scroll
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+
+    // imagesIds.add(lastId + 1);
+    // imagesIds.add(lastId + 2);
+    // imagesIds.add(lastId + 3);
+    // imagesIds.add(lastId + 4);
+    // imagesIds.add(lastId + 5);
+
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +79,7 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
+            controller: scrollController,
             itemCount: imagesIds.length,
             itemBuilder: (context, index) {
               return FadeInImage(
@@ -33,11 +91,14 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
               );
             }),
       ),
-      floatingActionButton:
-          FloatingActionButton(
-            onPressed: () => context.pop(),
-            child: const Icon(Icons.arrow_back_ios_new_sharp),
-            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        // child: const Icon(Icons.arrow_back_ios_new_sharp),
+        child: (!isLoading)
+            ? FadeIn(child: const Icon(Icons.arrow_back_ios_new_sharp)) //con el fade in, no aparace de golpe el boton cuando se termina de cargar
+            : SpinPerfect(
+                infinite: true, child: const Icon(Icons.refresh_rounded)),
+      ),
     );
   }
 }
